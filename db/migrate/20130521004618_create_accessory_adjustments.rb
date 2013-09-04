4b5cb60147b418197d736d32915a64d69e9da676
Jianbo Su <sujianbo@gmail.com>
2013-09-04 14:16:08 +0800

    add default to create accessory adjustment

--- db/migrate/20130521004618_create_accessory_adjustments.rb
+++ db/migrate/20130521004618_create_accessory_adjustments.rb
@@ -1,16 +1,16 @@
 class CreateAccessoryAdjustments < ActiveRecord::Migration
   def change
     create_table :accessory_adjustments do |t|
-      t.date :effective_date
+      t.date :effective_date, default: Time.now
       t.integer :created_by_id
       t.integer :updated_by_id
       t.integer :submitted_by_id
       t.datetime :submitted_at
-      t.boolean :submitted
-      t.boolean :confirmed
+      t.boolean :submitted, default: false
+      t.boolean :confirmed, default: false
       t.integer :confirmed_by_id
       t.datetime :confirmed_at
-      t.boolean :approved
+      t.boolean :approved, default: false
       t.datetime :approved_at
       t.integer :approved_by_id
 
