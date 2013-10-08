58771f7215d0ec7a4c03a6f9c47da4ce1d03f926
Jianbo Su <sujianbo@gmail.com>
2013-10-08 10:31:31 +0800

    fix migration typo

--- db/migrate/20130918042151_add_department_id_and_cost_center_id_to_users.rb
+++ db/migrate/20130918042151_add_department_id_and_cost_center_id_to_users.rb
@@ -1,6 +1,6 @@
 class AddDepartmentIdAndCostCenterIdToUsers < ActiveRecord::Migration
   def change
     add_column :users, :department_id, :integer
-    add_column :usres, :cost_center_id, :integer
+    add_column :users, :cost_center_id, :integer
   end
 end
